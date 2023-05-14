f6cfc48fbcbf7afb2d55bb991eb5d396268bfc29
Denisov-Egor <129548353+Denisov-Egor@users.noreply.github.com>
2023-05-14 20:04:06 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "RU");
+    int decimal_num;
+    cout << "Введите десятичное число: ";
+    cin >> decimal_num;
+
+    string hex_num;
+
+    while (decimal_num > 0) {
+        int remainder = decimal_num % 16;
+        if (remainder < 10) {
+            hex_num.insert(0, to_string(remainder));
+        }
+        else {
+            hex_num.insert(0, 1, char('A' + remainder - 10));
+        }
+        decimal_num /= 16;
+    }
+
+    cout << "Результат: " << hex_num << endl;
+
+    return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "RU");
+    string input_str;
+    int word_count = 0;
+
+    cout << "Введите строку: ";
+    getline(cin, input_str);
+
+    stringstream ss(input_str);
+
+    while (ss >> input_str) {
+        word_count++;
+    }
+
+    cout << "Количество слов: " << word_count << endl;
+
+    return 0;
+}
